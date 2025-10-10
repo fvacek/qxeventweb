63d4b81b873879976069958f7fcbcfb51b2ecb1d
Fanda Vacek <fvacek@elektroline.cz>
2025-10-10 17:33:13 +0200

    Add google open-id client ID

--- src/auth/auth-config.ts
+++ src/auth/auth-config.ts
@@ -2,7 +2,7 @@ import { UserManagerSettings } from "oidc-client-ts";
 
 export const googleOidcConfig: UserManagerSettings = {
   authority: "https://accounts.google.com",
-  client_id: "YOUR_GOOGLE_CLIENT_ID.apps.googleusercontent.com",
+  client_id: "921803215801-igjbc90peabavthblhsmo1da37bo9r4l.apps.googleusercontent.com",
   redirect_uri: "http://localhost:3000/auth/callback/google",
   scope: "openid profile email",
   response_type: "code",
