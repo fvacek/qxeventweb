4dfa211b1544ffe529aef37a03c960790f1567e2
Fanda Vacek <fanda.vacek@gmail.com>
2026-07-05 20:21:23 +0200

    Preserve qxchange data on status updates

--- src/components/Runs.tsx
+++ src/components/Runs.tsx
@@ -226,11 +226,11 @@ function applyRecChngToRuns(runs: Run[], recchng: RecChng, mode: RunsMode): Run[
     }
 
     case SqlOperation.Update: {
-      if (record?.status && record.status !== "Pending") {
-        console.log(`processRecChng: [RESOLVED] Removing qxchange data from run with qxchange_id=${id}`);
-        // hide required changes on status update, because change is accepted or rejected already
-        return clearQxChange(runs, id);
-      }
+      // if (record?.status && record.status !== "Pending") {
+      //   console.log(`processRecChng: [RESOLVED] Removing qxchange data from run with qxchange_id=${id}`);
+      //   // hide required changes on status update, because change is accepted or rejected already
+      //   clearQxChange(runs, id);
+      // }
 
       const lateEntry = parseLateEntryFromRecChng(record);
 
@@ -238,7 +238,7 @@ function applyRecChngToRuns(runs: Run[], recchng: RecChng, mode: RunsMode): Run[
         r.qxchange_id === id
           ? {
             ...r,
-            qxchange_user_id: str(record?.user_id, r.qxchange_user_id),
+            // qxchange_user_id: str(record?.user_id, r.qxchange_user_id),
             qxchange_status: str(record?.status, r.qxchange_status),
             qxchange_status_message: str(record?.status_message, r.qxchange_status_message),
             qxchange_data: lateEntry !== undefined ? { LateEntry: lateEntry } : r.qxchange_data,
